7ff05e0fb2fdd034c9d267d038570bd7321e0a9c
yapcheahshen <yapcheahshen@gmail.com>
2017-02-26 10:47:10 +0800

    minor changes

--- p/ksanalibindex.js
+++ p/ksanalibindex.js
@@ -1,5 +1,11 @@
-global.KsanaCorpusBuilder=require("ksana-corpus-builder");
-global.KsanaCorpus=require("ksana-corpus");
-global.KsanaLocalization=require("ksana-localization");
-global.KsanaCorpusSearch=require("ksana-corpus-search");
-global.KsanaJsonrom=require("ksana-jsonrom");
+if (typeof global!=="undefined") {
+	global.KsanaCorpusBuilder=require("ksana-corpus-builder");
+	global.KsanaCorpus=require("ksana-corpus");
+	global.KsanaCorpusSearch=require("ksana-corpus-search");
+	global.KsanaJsonrom=require("ksana-jsonrom");	
+} else if (typeof window!=="undefined"){
+	window.KsanaCorpusBuilder=require("ksana-corpus-builder");
+	window.KsanaCorpus=require("ksana-corpus");
+	window.KsanaCorpusSearch=require("ksana-corpus-search");
+	window.KsanaJsonrom=require("ksana-jsonrom");		
+}
--- p/makeksanalib.cmd
+++ p/makeksanalib.cmd
@@ -1,2 +1,2 @@
-browserify ksanalibindex.js --bare -x codemirror -x react -x react-dom -u react-native -o ksanalib.js
-uglifyjs ksanalib.js > ksanalib.min.js
+rem browserify ksanalibindex.js --bare -x codemirror -x react -x react-dom -u react-native  > ksanalib.js
+browserify ksanalibindex.js --bare -x codemirror -x react -x react-dom -u react-native | uglifyjs -m -c=dead_code,evaluate,loops,unused > ksanalib.min.js
