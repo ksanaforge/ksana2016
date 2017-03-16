2fcd91e9571cdf41855a1afc636755514dc4ec98
yapcheahshen <yapcheahshen@gmail.com>
2017-03-16 23:08:04 +0800

    copy ksanalib.min.js to ksana-corpus-lib

--- p/makeksanalib.cmd
+++ p/makeksanalib.cmd
@@ -1,3 +1,3 @@
 rem browserify ksanalibindex.js --bare -x codemirror -x react -x react-dom -u react-native  > ksanalib.js
 browserify ksanalibindex.js --bare -x codemirror -x react -x react-dom -u react-native | uglifyjs -m -c=dead_code,evaluate,loops,unused > ksanalib.min.js
-rem copy to node_modules/ksana-corpus-lib/
+copy/y ksanalib.min.js node_modules\ksana-corpus-lib\
--- p/mobx-boilderplate/src/index.js
+++
@@ -1,9 +0,0 @@
-const store=require("./store");
-const React=require("react");
-const ReactDOM=require("react-dom");
-const E=React.createElement;
-const {useStrict}=require("mobx");
-const TodoList=require("./todolist");
-
-//useStrict(true)
-ReactDOM.render(E(TodoList,{store}),document.getElementById("root"));
