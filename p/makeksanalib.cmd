839e346185af7f654f516dbd69659f979eba3d60
yapcheahshen <yapcheahshen@gmail.com>
2017-03-30 17:48:29 +0800

    change makeksanalib

--- p/makeksanalib.cmd
+++ p/makeksanalib.cmd
@@ -1,3 +1,4 @@
+rem uglify only support es5
 rem browserify ksanalibindex.js --bare -x codemirror -x react -x react-dom -u react-native  > ksanalib.js
 browserify ksanalibindex.js --bare -x codemirror -x react -x react-dom -u react-native | uglifyjs -m -c=dead_code,evaluate,loops,unused > ksanalib.min.js
-copy/y ksanalib.min.js node_modules\ksana-corpus-lib\
+copy/y ksanalib.min.js ..\node_modules\ksana-corpus-lib\
---
+++ p/makeksanalib_dev.cmd
@@ -0,0 +1,5 @@
+rem uglify only support es5
+rem browserify ksanalibindex.js --bare -x codemirror -x react -x react-dom -u react-native  > ksanalib.js
+rem developer version
+browserify ksanalibindex.js --bare -x codemirror -x react -x react-dom -u react-native | uglifyjs -m -c=dead_code,evaluate,loops,unused > ksanalib.min.js
+copy/y ksanalib.min.js node_modules\ksana-corpus-lib\
