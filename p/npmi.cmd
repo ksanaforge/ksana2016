37493cfe53e62fcbb60865e9d502972d68c5ff4a
yapcheahshen <yapcheahshen@gmail.com>
2017-04-01 10:15:59 +0800

    add cmd for install modules

---
+++ p/npmi.cmd
@@ -0,0 +1,9 @@
+REM incase npm cannot install from git
+mkdir node_modules
+cd node_modules
+git clone https://github.com/ksanaforge/ksana-localization
+git clone https://github.com/ksanaforge/accelon2017
+git clone https://github.com/ksanaforge/ksana2015-breadcrumbtoc
+git clone https://github.com/ksanaforge/ksana-codemirror
+git clone https://github.com/ksanaforge/ksana-unihan-variant
+git clone https://github.com/ksanaforge/ksana-corpus-view
