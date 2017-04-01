20bbef8795a9b9155c0f3142d45924c1cf223165
yapcheahshen <yapcheahshen@gmail.com>
2017-04-01 10:29:41 +0800

    add script to install ksana source

---
+++ p/npmi-dev.cmd
@@ -0,0 +1,5 @@
+REM make sure you have access to these repository
+cd node_modules
+git clone https://github.com/ksanaforge/ksana-corpus
+git clone https://github.com/ksanaforge/ksana-corpus-rom
+git clone https://github.com/ksanaforge/ksana-corpus-search
