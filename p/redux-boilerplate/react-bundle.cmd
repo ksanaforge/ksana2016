0a398fe98196e5412a1c04fc79973fd7ed4738ae
yapcheahshen <yapcheahshen@gmail.com>
2017-04-22 22:29:25 +0800

    remove redux

--- p/redux-boilerplate/react-bundle.cmd
+++
@@ -1,1 +0,0 @@
-browserify -r react -r react-dom  | uglifyjs --screw-ie8 -c=dead_code,evaluate,loops,unused > static/react-bundle.min.js
--- p/redux-boilerplate/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-const { combineReducers } =require('redux');
-const counter =require('./counter');
-
-const rootReducer = combineReducers({
-  counter
-});
-
-module.exports=rootReducer;
